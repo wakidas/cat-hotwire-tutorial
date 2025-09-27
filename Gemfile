c2e7fa9e7240533be65945adc3e7c082c9042521
Shimpei Wakida <[shimpei.wakida@gmail.com>
2025-09-27 22:16:44 +0900

    サイドバー実装

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem "thruster", require: false
 
 gem "kaminari"
 gem "ransack"
+gem "bootstrap_form"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.ransackable_attributes(auth_object = nil)
     ["age", "created_at", "id", "name", "updated_at"]
   end
