a0ef98d54f2d6e61486bb5e9dcb448eb29da5702
Shimpei Wakida <[shimpei.wakida@gmail.com>
2025-09-28 14:33:34 +0900

    編集のturbo stream化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -42,7 +42,6 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ねこを更新しました。", status: :see_other
     else
       render :edit, status: :unprocessable_content
     end
